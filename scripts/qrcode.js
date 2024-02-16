51f6156ed4de05758f5294a134810222b97907c2
hosseinGith <hosseindrees7@gmail.com>
2024-02-16 21:18:34 +0330

    *

--- scripts/qrcode.js
+++ scripts/qrcode.js
@@ -2,23 +2,28 @@ const qrCodeFiles = document.querySelector("#qrCodeFiles");
 const qrcodeBtn = document.querySelector("#qrCodeReader");
 
 qrcodeBtn.addEventListener("click", () => {});
-let video = document.createElement("video");
-video.setAttribute("playsinline", "");
-video.setAttribute("autoplay", "");
-video.setAttribute("muted", "");
-video.style = "width:100%;height:100%;position:fixed;top:0;left:0";
 
-/* Setting up the constraint */
-let facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
-let constraints = {
-  audio: false,
-  video: {
-    facingMode: facingMode,
-  },
-};
-/* Stream it to video element */
-navigator.mediaDevices.getUserMedia(constraints).then(function success(stream) {
-  video.srcObject = stream;
-  alert(video.srcObject);
-});
-document.appendChild(video);
+function startSteam() {
+  let video = document.createElement("video");
+  video.setAttribute("playsinline", "");
+  video.setAttribute("autoplay", "");
+  video.setAttribute("muted", "");
+  video.style = "width:100%;height:100%;position:fixed;top:0;left:0";
+
+  /* Setting up the constraint */
+  let facingMode = "user"; // Can be 'user' or 'environment' to access back or front camera (NEAT!)
+  let constraints = {
+    audio: false,
+    video: {
+      facingMode: facingMode,
+    },
+  };
+  /* Stream it to video element */
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function success(stream) {
+      video.srcObject = stream;
+      alert(video.srcObject);
+    });
+  document.body.appendChild(video);
+}
