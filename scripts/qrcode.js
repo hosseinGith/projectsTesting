d4659acbdb074039be1e08a753577df81337d189
hosseinGith <hosseindrees7@gmail.com>
2024-02-16 21:36:22 +0330

    *

--- scripts/qrcode.js
+++ scripts/qrcode.js
@@ -1,8 +1,14 @@
 const qrCodeFiles = document.querySelector("#qrCodeFiles");
 const qrcodeBtn = document.querySelector("#qrCodeReader");
 
-qrcodeBtn.addEventListener("click", () => {});
-
+qrcodeBtn.addEventListener("click", () => {
+  startSteam();
+});
+function qrCodeFun(img) {
+  QCodeDecoder().decodeFromImage(img, function (er, res) {
+    alert(utf8.decode(res));
+  });
+}
 async function startSteam() {
   let video = document.createElement("video");
   video.setAttribute("playsinline", "");
@@ -18,13 +24,15 @@ async function startSteam() {
       facingMode: facingMode,
     },
   };
+
   /* Stream it to video element */
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then(function success(stream) {
-      video.srcObject = stream;
-      alert(video.srcObject);
+      urlImg = video.srcObject = stream;
+      setInterval(() => {
+        qrCodeFun(video.srcObject);
+      });
     });
   document.body.appendChild(video);
 }
-startSteam();
