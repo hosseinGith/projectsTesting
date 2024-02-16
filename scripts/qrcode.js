c9431443cc3b22eccbdfa728c053ef8829878e29
hosseinGith <hosseindrees7@gmail.com>
2024-02-16 20:41:56 +0330

    *

--- scripts/qrcode.js
+++ scripts/qrcode.js
@@ -4,21 +4,6 @@ const qrcodeBtn = document.querySelector("#qrCodeReader");
 function startScan() {
   function onScanSuccess(decodedText, decodedResult) {
     alert(decodedText, decodedResult);
-    let reqBtn = document.querySelector("#reader");
-    reqBtn.style = `
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    border: 0;
-    `;
-    reqBtn.querySelector("video").controls = false;
-    reqBtn.querySelectorAll("button").forEach((item, index) => {
-      item.style = `display:none;`;
-    });
-    reqBtn.querySelector("select").style = `display:none;`;
-    reqBtn.querySelector(">div:first-child").style = `display:none;`;
-    reqBtn.querySelector("img").style = `display:none;`;
-    reqBtn.querySelector("#reader__dashboard").style = `display:none;`;
   }
 
   function onScanError(errorMessage) {}
@@ -32,8 +17,25 @@ function startScan() {
 }
 startScan();
 
+let reqBtn = document.querySelector("#reader__camera_permission_button");
+reqBtn.click();
 qrcodeBtn.addEventListener("click", () => {
-  let reqBtn = document.querySelector("#reader__camera_permission_button");
+  let reader = document.querySelector("#reader");
+  let startBtn = document.querySelector("#reader button");
+  reader.style = `
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    border: 0;
+    `;
+  reader.querySelector("video").controls = false;
+  reader.querySelectorAll("button").forEach((item, index) => {
+    item.style = `display:none;`;
+  });
+  reader.querySelector("select").style = `display:none;`;
+  reader.querySelector(">div:first-child").style = `display:none;`;
+  reader.querySelector("img").style = `display:none;`;
+  reader.querySelector("#reader__dashboard").style = `display:none;`;
   // qrCodeFiles.click();
-  reqBtn.click();
+  startBtn.click();
 });
