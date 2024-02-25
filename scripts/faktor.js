f9a0045e55ad01a692dd6d47a4edaf084bed7b2c
hosseinGith <hosseindrees7@gmail.com>
2024-02-25 09:54:32 +0330

    *

--- scripts/faktor.js
+++ scripts/faktor.js
@@ -1,16 +1,19 @@
-const doc = new jsPDF();
-const screenshotTarget = document.querySelector("#screenShot");
-html2canvas(screenshotTarget).then((canvas) => {
-  let dataURL = canvas.toDataURL();
-  new jsPDF({
-    orientation: "l",
-    unit: "pt",
-    format: "a1",
-  });
-  let a = document.createElement("a");
-  a.href = dataURL;
-  a.download = "asd.png";
-  a.click();
-  doc.addImage(dataURL, "JPEG", 0, 0, canvas.width, canvas.height);
-  doc.save("asd.pdf");
+$(document).ready(function ($) {
+  var ua = navigator.userAgent.toLowerCase();
+  var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+
+  if (isAndroid) {
+    // https://developers.google.com/cloud-print/docs/gadget
+    var gadget = new cloudprint.Gadget();
+    gadget.setPrintDocument(
+      "url",
+      $("title").html(),
+      window.location.href,
+      "utf-8"
+    );
+    gadget.openPrintDialog();
+  } else {
+    window.print();
+  }
+  return false;
 });
