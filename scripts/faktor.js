d6547e2974d26e1556db395cca0cff86aca05fab
hosseinGith <hosseindrees7@gmail.com>
2024-02-25 10:06:35 +0330

    *

--- scripts/faktor.js
+++ scripts/faktor.js
@@ -1,19 +1,7 @@
-$(document).ready(function ($) {
-  var ua = navigator.userAgent.toLowerCase();
-  var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
-
-  if (isAndroid) {
-    // https://developers.google.com/cloud-print/docs/gadget
-    var gadget = new cloudprint.Gadget();
-    gadget.setPrintDocument(
-      "url",
-      $("title").html(),
-      window.location.href,
-      "utf-8"
-    );
-    gadget.openPrintDialog();
-  } else {
-    window.print();
-  }
-  return false;
-});
+var gadget = new cloudprint.Gadget();
+gadget.setPrintButton(cloudprint.Gadget.createDefaultPrintButton("gcpPrint"));
+gadget.setPrintDocument(
+  "text/html",
+  "Print",
+  document.documentElement.innerHTML
+);
